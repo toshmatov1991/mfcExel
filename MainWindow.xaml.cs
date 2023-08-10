d9aa9be8074b49a83c473653c79dd1c6c5644897
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-08-10 12:29:50 +0700

    Верстка окно - финал

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using exel_for_mfc.PassModels;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -16,21 +18,69 @@ using System.Windows.Shapes;
 
 namespace exel_for_mfc
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+        
 
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //При нажатии на кнопку Войти
+            //Вход
+            //Проверка на пустые поля
+            //Проверка входных значений
+            if (string.IsNullOrWhiteSpace(login_text.Text)
+            && string.IsNullOrWhiteSpace(password_text.Password)
+            || string.IsNullOrWhiteSpace(login_text.Text)
+            || string.IsNullOrWhiteSpace(password_text.Password))
+            {
+                MessageBox.Show("Не заполнено одно или несколько полей");
+                if (string.IsNullOrWhiteSpace(login_text.Text))
+                    Keyboard.ClearFocus();
+           
+                if (string.IsNullOrWhiteSpace(password_text.Password))
+                    Keyboard.ClearFocus();
+                
+            }
+            else
+            {
+                using (PassContext db = new())
+                {
+                    var GetAllLogPass = await db.Passwords.ToListAsync();
+                    int temp = 0;
+                    foreach (var item in GetAllLogPass)
+                    {
+                        if (item.Login == login_text.Text && item.Pass == password_text.Password)
+                        {
+                            //temp++;
+                            //User user = new($"{item.Firstname} {item.Name} {item.Lastname}", (int)item.Id);
+                            //user.Show();
+                            //Close();
+                            //break;
+                        }
+                    }
+                    if (temp == 0)
+                    {
+                        MessageBox.Show("Повторите попытку", "Неправильный логин или пароль", MessageBoxButton.OK, MessageBoxImage.Stop);
+                    }
+                }
+            }
+        }
 
+        //private void GoOpen(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Enter)
+        //        Button_Click(sender, e);
+        //}
 
-
-
-
+        //private void GoOpenLog(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Enter)
+        //        Button_Click(sender, e);
+        //}
 
 
 
