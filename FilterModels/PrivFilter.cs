7d534041cc8cf9f791f9793c4a755030db82c00f
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-10-14 21:44:44 +0700

    456

--- FilterModels/PrivFilter.cs
+++ FilterModels/PrivFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Bibliography;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace exel_for_mfc.FilterModels
 {
-    internal class PrivFilter
+    public class PrivFilter
     {
+        public int Id { get; set; }
+
+        public string? PrivilegesName { get; set; }
+
+        public int? PrivBool { get; set; }
+
+        public PrivFilter(int id, string? priv, int? privBool)
+        {
+            Id = id;
+            PrivilegesName = priv;
+            PrivBool = privBool;
+        }
     }
 }
--- FilterModels/SolFilter.cs
+++ FilterModels/SolFilter.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace exel_for_mfc.FilterModels
 {
-    internal class SolFilter
+    public class SolFilter
     {
+        public int Id { get; set; }
+        public string? SolutionName { get; set; }
+
+        public int? SolBool { get; set; }
+
+        public SolFilter(int id, string? sol, int? solBool)
+        {
+            Id = id;
+            SolutionName = sol;
+            SolBool = solBool;
+        }
     }
 }
