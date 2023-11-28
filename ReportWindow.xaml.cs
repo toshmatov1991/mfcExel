b9b191537ac9618d7573adafaf74b00018212f89
toshmatov1991 <kamentimur1991@gmail.com>
2023-11-28 10:40:35 +0700

    Вело Вело

--- ReportWindow.xaml.cs
+++ ReportWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace exel_for_mfc
 {
     public partial class ReportWindow : Window
     {
+        private bool flag = true;
 
         private DateTime yearCodeBehind = DateTime.Today;
 
@@ -50,6 +51,7 @@ namespace exel_for_mfc
         {
 
             #region Стили
+            //Стиль главного заголовка
             SLStyle titleStyle = new SLStyle();
             titleStyle.Font.FontName = "Arial";
             titleStyle.Font.FontSize = 16;
@@ -57,6 +59,20 @@ namespace exel_for_mfc
             titleStyle.SetWrapText(true);
             titleStyle.SetVerticalAlignment(DocumentFormat.OpenXml.Spreadsheet.VerticalAlignmentValues.Center);
             titleStyle.SetHorizontalAlignment(DocumentFormat.OpenXml.Spreadsheet.HorizontalAlignmentValues.Center);
+
+            //Стиль месяца
+            SLStyle itemRowHeaderStyle = new SLStyle();
+            itemRowHeaderStyle.Font.FontName = "Arial";
+            itemRowHeaderStyle.Font.FontSize = 14;
+            itemRowHeaderStyle.SetWrapText(true);
+            titleStyle.Font.Bold = true;
+            itemRowHeaderStyle.SetVerticalAlignment(DocumentFormat.OpenXml.Spreadsheet.VerticalAlignmentValues.Center);
+            itemRowHeaderStyle.SetHorizontalAlignment(DocumentFormat.OpenXml.Spreadsheet.HorizontalAlignmentValues.Center);
+            itemRowHeaderStyle.Border.BottomBorder.BorderStyle = itemRowHeaderStyle.Border.TopBorder.BorderStyle = itemRowHeaderStyle.Border.LeftBorder.BorderStyle = itemRowHeaderStyle.Border.RightBorder.BorderStyle = DocumentFormat.OpenXml.Spreadsheet.BorderStyleValues.Thin;
+            itemRowHeaderStyle.Border.BottomBorder.Color = itemRowHeaderStyle.Border.TopBorder.Color = itemRowHeaderStyle.Border.LeftBorder.Color = itemRowHeaderStyle.Border.RightBorder.Color = System.Drawing.Color.Black;
+
+
+
             #endregion
 
 
@@ -64,17 +80,35 @@ namespace exel_for_mfc
 
             if (str != string.Empty)
             {
-                //Получил список выбранных Месяцев
+                // Создаю документ
+                using SLDocument doc = new();
+
+
+                // Получил список выбранных Месяцев
                 var listMouth = ListMouth();
 
-                //Генерация колонок в зависимости от выбора Месяцев
-                //Создаю объкт таблицы
+                // Генерация колонок в зависимости от выбора Месяцев
+                // Создаю объкт таблицы
                 DataTable dt = new();
 
                 //Затем в цикле надо задать колонки Месяцев
                 foreach (var item in listMouth)
                 {
                     dt.Columns.Add(item, typeof(string));
+                    
+                }
+
+                // Задать стиль района Главного Заголовка
+                doc.SetColumnWidth(1, 35);
+                doc.SetRowHeight(1, 30);
+                doc.SetCellStyle(1, 1, titleStyle);
+
+
+                // Задать стили заголовков месяцев колонок
+                for (int j = 2; j < listMouth.Count + 1; j++)
+                {
+                   doc.SetColumnWidth(j, 15);
+                   doc.SetCellStyle(1, j, itemRowHeaderStyle);
                 }
 
 
@@ -86,7 +120,7 @@ namespace exel_for_mfc
 
 
                 using ExDbContext db = new();
-                using SLDocument doc = new();
+
                 doc.ImportDataTable(1, 1, dt, true);
 
                 var getMyArea = db.Areas.Where(u => u.HidingArea == 1).OrderBy(u => u.AreaName).ToList();
@@ -94,9 +128,10 @@ namespace exel_for_mfc
 
                 //doc.SetCellValue("A1", "Район");
 
-                doc.SetColumnWidth(1, 40);
-                doc.SetRowHeight(1, 30);
-                doc.SetCellStyle(1, 1, titleStyle);
+              
+
+
+                
 
                 int i = 2;
                 foreach (var item in getMyArea)
@@ -183,7 +218,51 @@ namespace exel_for_mfc
             yearCodeBehind = yearCodeBehind.AddYears(1);
             TotalAmountForAllTime.Text = yearCodeBehind.Year.ToString();
         }
+
+        //Галочки поставить все и убрать
+        private void AllCheckOrNo_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+            {
+                checkBox1.IsChecked = true;
+                checkBox2.IsChecked = true;
+                checkBox3.IsChecked = true;
+                checkBox4.IsChecked = true;
+                checkBox5.IsChecked = true;
+                checkBox6.IsChecked = true;
+                checkBox7.IsChecked = true;
+                checkBox8.IsChecked = true;
+                checkBox9.IsChecked = true;
+                checkBox10.IsChecked = true;
+                checkBox11.IsChecked = true;
+                checkBox12.IsChecked = true;
+                allCheckOrNo.Content = "Убрать все галочки";
+                flag = false;
+            }
+            
+            else if (!flag)
+            {
+                checkBox1.IsChecked = false;
+                checkBox2.IsChecked = false;
+                checkBox3.IsChecked = false;
+                checkBox4.IsChecked = false;
+                checkBox5.IsChecked = false;
+                checkBox6.IsChecked = false;
+                checkBox7.IsChecked = false;
+                checkBox8.IsChecked = false;
+                checkBox9.IsChecked = false;
+                checkBox10.IsChecked = false;
+                checkBox11.IsChecked = false;
+                checkBox12.IsChecked = false;
+                allCheckOrNo.Content = "Отметить все галочки";
+                flag = true;
+            }
+
+        }
+
+
         #endregion
+
 
     }
 }
